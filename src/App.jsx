c8ea9d4d7200e4f444b2c75b069a9f25695038fd
Eli Schiffler <eschiffler1122@gmail.com>
2025-06-11 18:43:10 -0600

    created basic format

--- src/App.jsx
+++ src/App.jsx
@@ -2,32 +2,19 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import { DiReact, DiHtml5, DiJavascript1 } from 'react-icons/di'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div className="header">
+        <h1 style={{ marginBottom: 0 }}>Eli Schiffler</h1>
+        <h2 style={{ marginTop: 0, fontSize: '1.2rem', fontWeight: 'normal' }}>
+          Software Developer / Quantum Computing
+        </h2>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
