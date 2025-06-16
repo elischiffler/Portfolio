a2ccec108df6ffb9b1ff2e819a8dfc34b5eb4404
Eli Schiffler <eschiffler1122@gmail.com>
2025-06-15 20:49:08 -0600

    Created animated background

---
+++ src/components/CosmicDustBackground.jsx
@@ -0,0 +1,11 @@
+import { AnimatedBackground } from 'animated-backgrounds';
+
+function CosmicDustBackground() {
+  return (
+    <div>
+      <AnimatedBackground animationName="cosmicDust" theme="portfolio" />
+    </div>
+  );
+}
+
+export default CosmicDustBackground;
