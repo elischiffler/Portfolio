832bdaf5fd318b06faa3827c27e947b3a5404dab
Eli Schiffler <eschiffler1122@gmail.com>
2026-03-10 16:07:30 -0700

    changed profile card colors to match theme

--- src/components/ProfileCard/ProfileCard.jsx
+++ src/components/ProfileCard/ProfileCard.jsx
@@ -14,10 +14,10 @@ import {
 import './ProfileCard.css';
 
 const DEFAULT_BEHIND_GRADIENT =
-  'radial-gradient(farthest-side circle at var(--pointer-x) var(--pointer-y),hsla(266,100%,90%,var(--card-opacity)) 4%,hsla(266,50%,80%,calc(var(--card-opacity)*0.75)) 10%,hsla(266,25%,70%,calc(var(--card-opacity)*0.5)) 50%,hsla(266,0%,60%,0) 100%),radial-gradient(35% 52% at 55% 20%,#00ffaac4 0%,#073aff00 100%),radial-gradient(100% 100% at 50% 50%,#00c1ffff 1%,#073aff00 76%),conic-gradient(from 124deg at 50% 50%,#c137ffff 0%,#07c6ffff 40%,#07c6ffff 60%,#c137ffff 100%)';
+  'radial-gradient(farthest-side circle at var(--pointer-x) var(--pointer-y),hsla(215,100%,90%,var(--card-opacity)) 4%,hsla(215,50%,80%,calc(var(--card-opacity)*0.75)) 10%,hsla(215,25%,70%,calc(var(--card-opacity)*0.5)) 50%,hsla(215,0%,60%,0) 100%),radial-gradient(35% 52% at 55% 20%,#7eb8d6c4 0%,#93a6c000 100%),radial-gradient(100% 100% at 50% 50%,#93a6c0ff 1%,#93a6c000 76%),conic-gradient(from 124deg at 50% 50%,#93a6c0ff 0%,#7eb8d6ff 40%,#7eb8d6ff 60%,#93a6c0ff 100%)';
 
 const DEFAULT_INNER_GRADIENT =
-  'linear-gradient(145deg,#60496e8c 0%,#71C4FF44 100%)';
+  'linear-gradient(145deg,#1a202c8c 0%,#93a6c044 100%)';
 
 const ANIMATION_CONFIG = {
   SMOOTH_DURATION: 600,
